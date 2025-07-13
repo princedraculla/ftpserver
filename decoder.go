3cc24490911e75d91cdfb729f00d4a2a1d0c509f
princedraculla <amirtorkashvand56@gmail.com>
2025-07-13 14:43:53 +0330

    unmarshaling asn1 and marshaling to string using json and spew package

--- decoder.go
+++ decoder.go
@@ -2,107 +2,15 @@ package main
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/princedraculla/ftpservertest/asn"
+	"github.com/princedraculla/ftpservertest/cdrType"
 )
-
-var (
-	cdrStorage = "/home/amir/ftp_files/var/CDRs/1"
-)
-
-type TsNumberIdentifier uint8
-
-const (
-	TS32005 TsNumberIdentifier = 0
-	TS32015 TsNumberIdentifier = 1
-	TS32205 TsNumberIdentifier = 2
-	TS32215 TsNumberIdentifier = 3
-	TS32225 TsNumberIdentifier = 4
-	TS32235 TsNumberIdentifier = 5
-	TS32250 TsNumberIdentifier = 6
-	TS32251 TsNumberIdentifier = 7
-	TS32260 TsNumberIdentifier = 9
-	TS32270 TsNumberIdentifier = 10
-	TS32271 TsNumberIdentifier = 11
-	TS32272 TsNumberIdentifier = 12
-	TS32273 TsNumberIdentifier = 13
-	TS32275 TsNumberIdentifier = 14
-	TS32274 TsNumberIdentifier = 15
-	TS32277 TsNumberIdentifier = 16
-	TS32296 TsNumberIdentifier = 17
-	TS32278 TsNumberIdentifier = 18
-	TS32253 TsNumberIdentifier = 19
-	TS32255 TsNumberIdentifier = 20
-	TS32254 TsNumberIdentifier = 21
-	TS32256 TsNumberIdentifier = 22
-	TS28201 TsNumberIdentifier = 23
-	TS28202 TsNumberIdentifier = 24
-)
-
-type DataRecordFormatType uint8
-
-const (
-	BasicEncodingRules DataRecordFormatType = iota + 1
-	UnalignedPackedEncodingRules
-	AlignedPackedEncodingRules1
-	XMLEncodingRules
-)
-
-type CDRFile struct {
-	Hdr     CdrFileHeader
-	CdrList []CDR
-}
-type CDR struct {
-	Hdr     CDRHeader
-	CdrByte []byte
-}
-
-type CdrFileHeader struct {
-	FileLength                            uint32
-	HeaderLength                          uint32
-	HighReleaseIdentifier                 uint8 // octet 9 bit 6..8
-	HighVersionIdentifier                 uint8 // octet 9 bit 1..5
-	LowReleaseIdentifier                  uint8 // octet 10 bit 6..8
-	LowVersionIdentifier                  uint8 // octet 10 bit 1..5
-	FileOpeningTimeStamp                  CdrHdrTimeStamp
-	TimestampWhenLastCdrWasAppendedToFile CdrHdrTimeStamp
-	NumberOfCdrsInFile                    uint32
-	FileSequenceNumber                    uint32
-	FileClosureTriggerReason              FileClosureTriggerReasonType
-	IPAddressOfNodeThatGeneratedFile      [20]byte // ip address in ipv6 format
-	LostCdrIndicator                      uint8
-	LengthOfCdrRoutingFilter              uint16
-	CDRRoutingFiler                       []byte //vendor specific
-	LengthOfPrivateExtension              uint16
-	PrivateExtension                      []byte // vendor specific
-	HighReleaseIdentifierExtension        uint8
-	LowReleaseIdentifierExtension         uint8
-}
-
-type CDRHeader struct {
-	CdrLength                  uint16
-	ReleaseIdentifier          ReleaseIdentifierType // octect 3 bit 6..8
-	VersionIdentifier          uint8
-	DataRecordFormat           DataRecordFormatType
-	TsNumber                   TsNumberIdentifier
-	ReleaseIdentifierExtension uint8
-}
-type CdrHdrTimeStamp struct {
-	MonthLocal  uint8
-	DateLocal   uint8
-	HourLocal   uint8
-	MinuteLocal uint8
-
-	// bit set to "1" expresses "+" or bit set to "0" expresses "-" time deviation
-	SignOfTheLocalTimeDifferentialFromUtc uint8
-	HourDeviation                         uint8
-	MinuteDeviation                       uint8
-}
-
-type FileClosureTriggerReasonType uint8
-type ReleaseIdentifierType uint8
 
 func decodeCdrHdrTimeStamp(ts uint32) CdrHdrTimeStamp {
 	return CdrHdrTimeStamp{
@@ -217,6 +125,7 @@ func (c *CDRFile) DecodeCDRFile(filename string) (*CDRFile, error) {
 			return nil, fmt.Errorf("file too short for CDR %d", i)
 		}
 		var cdr CDR
+		var records cdrType.ChargingRecord
 		cdr.Hdr.CdrLength = binary.BigEndian.Uint16(data[offset : offset+2])
 		offset += 2
 		identifier := data[offset]
@@ -237,27 +146,22 @@ func (c *CDRFile) DecodeCDRFile(filename string) (*CDRFile, error) {
 		offset += int(cdr.Hdr.CdrLength)
 		cdrFile.CdrList = append(cdrFile.CdrList, cdr)
 		fmt.Printf("the CDR List : %d \n", cdrFile.CdrList[:])
+		if err := asn.UnmarshalWithParams(cdr.CdrByte, &records, "explicit,choice"); err != nil {
+			return nil, fmt.Errorf("error while unmarshaling cdrs : %v ", err)
+		}
+		jsonData, err := json.MarshalIndent(records, "", "  ")
+		if err != nil {
+			return nil, fmt.Errorf("marshaling cdrs to json error : %v", err)
+		}
+
+		fmt.Println("Unmarshaled CDRs : \n", string(jsonData))
+		//fmt.Println("unmarshaled CDRs : ", records)
+		spew.Dump("unmarshaled CDRs : ", records, "\n")
+
 	}
 	if uint32(len(data)) != h.FileLength {
 		log.Printf("Warning: File length mismatch, expected %d, got %d", h.FileLength, len(data))
 	}
 
 	return cdrFile, nil
-}
-
-func CDRFiles() ([]string, error) {
-	entries, err := os.ReadDir(cdrStorage)
-	if err != nil {
-		return nil, err
-	}
-	var files []string
-
-	for _, entrie := range entries {
-		if !entrie.IsDir() {
-			files = append(files, filepath.Join(cdrStorage, entrie.Name()))
-		}
-	}
-	fmt.Println("founded files : ", files)
-
-	return files, nil
 }
